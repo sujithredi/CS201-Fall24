82be36d265740043a6825efca037cbefada85738
Mareddy Joseph Sujith Reddy <78715711+sujithredi@users.noreply.github.com>
2025-03-28 21:40:59 -0500

    Update ST.hpp

--- Proj2/ST.hpp
+++ Proj2/ST.hpp
@@ -6,9 +6,9 @@
 #include "RBTPrint.hpp"
 
 /* 
-Name: Jay Kumar Roy
-Email: jroy4@crimson.ua.edu
-Course Section: Fall 2024 CS 201
+Name: Joseph Sujith Reddy Mareddy
+Email: jmareddy@crimson.ua.edu
+Course Section: Spring 2025 CS 201
 Homework #: 2
 */
 
@@ -16,23 +16,18 @@ template <class Key, class Type>
 class ST : public RedBlackTree<Key, Type> {
 public:
     typedef RBTNode<Key, Type>* iterator;
-
     // Constructors
-    // constant
     ST() {
         RedBlackTree<Key, Type>();
     }
 
     // Destructor
-    // linear in the size of the ST
     ~ST() {
         clear();
     }
-
     // access or insert specifies element
-    // inserts if the key is not present and returns a reference to
-    // the value corresponding to that key
-    // O(LogN), N size of ST
+    // inserts if the key is not present and returns a reference
+    // the value corresponding to that key O(LogN), N size of ST
     Type& operator[](const Key& key) {
         RBTNode<Key, Type>* node = RedBlackTree<Key, Type>::Search(key);
         if (node == nullptr) {
@@ -42,8 +37,7 @@ public:
     }
 
     // insert a (key, value) pair, if the key already exists
-    // set the new value to the existing key
-    // O(LogN), N size of ST
+    // set the new value to the existing key O(LogN), N size of ST
     void insert(const Key& key, const Type& value) {
         RBTNode<Key, Type>* node = RedBlackTree<Key, Type>::Search(key);
         if (node == nullptr) {
@@ -55,7 +49,6 @@ public:
     }
 
     // remove element at the given position
-    // amortized constant
     void remove(iterator position) {
         if (position != nullptr) {
             RedBlackTree<Key, Type>::RemoveNode(position);
@@ -63,27 +56,23 @@ public:
     }
 
     // remove element with keyvalue key and 
-    // return number of elements removed (either 0 or 1)
-    // O(logN), N size of ST
+    // return number of elements removed (either 0 or 1) O(logN), N size of ST
     std::size_t remove(const Key& key) {
         return RedBlackTree<Key, Type>::Remove(key);
     }
 
     // removes all elements from the ST, after this size() returns 0
-    // linear in the size of the ST
     void clear() {
         RedBlackTree<Key, Type>::DeleteTree(RedBlackTree<Key, Type>::root);
         RedBlackTree<Key, Type>::root = nullptr;
     }
 
     // checks if ST has no elements; true is empty, false otherwise
-    // constant
     bool empty() const {
         return RedBlackTree<Key, Type>::root == nullptr;
     }
 
     // returns number of elements in ST
-    // constant
     std::size_t size() const {
         if (RedBlackTree<Key, Type>::root == nullptr) {
             return 0;
@@ -94,41 +83,35 @@ public:
     }
 
     // returns number of elements that match keyvalue key
-    // value returned is 0 or 1 since keys are unique
-    // O(LogN), N size of ST
+    // value returned is 0 or 1 since keys are unique O(LogN), N size of ST
     std::size_t count(const Key& key) {
         return RedBlackTree<Key, Type>::Search(key) != nullptr ? 1 : 0;
     }
 
     // find an element with keyvalue key and return 
-    // the iterator to the element found, nullptr if not found
-    // O(LogN), N size of ST
+    // the iterator to the element found, nullptr if not found O(LogN), N size of ST
     iterator find(const Key& key) {
         return RedBlackTree<Key, Type>::Search(key);
     }
 
-    // check if key exists in ST
-    // O(LogN), N size of ST
+    // check if key exists in ST O(LogN), N size of ST
     bool contains(const Key& key) {
         return RedBlackTree<Key, Type>::Search(key) != nullptr;
     }
 
-    // return contents of ST as a vector of (key,value) pairs
-    // O(N), N size of ST
+    // return contents of ST as a vector of (key,value) pairs O(N), N size of ST
     std::vector<std::pair<Key, Type> > toVector() {
         std::vector<std::pair<Key, Type> > vec;
         toVectorRecursive(RedBlackTree<Key, Type>::root, vec);
         return vec;
     }
 
-    // print the symbol table as Red-Black Tree
-    // O(N), N size of ST
+    // print the symbol table as Red-Black Tree O(N), N size of ST
     void displayTree() {
         std::cout << RBTPrint<Key,Type>::TreeToString(RedBlackTree<Key,Type>::root) << std::endl;
     }
 
-    // print the symbol table in sorted order
-    // O(N), N size of ST
+    // print the symbol table in sorted order O(N), N size of ST
     void display() {
         displayRecursive(RedBlackTree<Key, Type>::root);
     }
